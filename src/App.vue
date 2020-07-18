3ba569b014c40d1a8c309cc7c2d773a3aad10a8a
PedroFariasSosa <pedro.farias.sosa@gmail.com>
2020-07-18 12:34:23 -0500

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Pokemon />
+  </div>
+</template>
+
+<script>
+import Pokemon from './components/Pokemon.vue'
+
+export default {
+  components: {
+    Pokemon
+  }
+}
+</script>
+
+<style>
+@import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
